0193989ee17b11f73858734504c756d88a888659
Mark <msavushkin@yandex.ru>
2022-07-02 10:09:38 +0300

    Внёс все правки согласно CodeStyle.

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -7,7 +7,6 @@ public class DepositCalculator {
 
     static double calculateComplexPercent(double a, double y, int d) {
         double pay = a * Math.pow((1 + y / 12), 12 * d);
-
         return pow(pay, 2);
     }
 
@@ -17,7 +16,6 @@ public class DepositCalculator {
 
     static double pow(double value, int places) {
         double scale = Math.pow(10, places);
-
         return Math.round(value * scale) / scale;
     }
 
@@ -26,27 +24,18 @@ public class DepositCalculator {
         int action;
 
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Введите сумму вклада в рублях:");
-
         int amount = scanner.nextInt();
-
         System.out.println("Введите срок вклада в годах:");
-
         period = scanner.nextInt();
-
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
-
         action = scanner.nextInt();
-
         double finalAmount = 0.0;
-
         if (action == 1) {
             finalAmount = calculateSimplePercent(amount, 0.06, period);
         } else if (action == 2) {
             finalAmount = calculateComplexPercent(amount, 0.06, period);
         }
-
         System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + finalAmount);
     }
 }
